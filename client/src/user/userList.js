913d221c4c85181eb70ab1683e6ca96234112d3a
premshani83 <prem.shani@successive.tech>
2019-10-18 15:37:16 +0530

    foramt

--- client/src/user/userList.js
+++ client/src/user/userList.js
@@ -1,8 +1,8 @@
-import React, {useCallback, useState} from 'react';
-import {gql} from 'apollo-boost';
+import React, { useCallback, useState } from 'react';
+import { gql } from 'apollo-boost';
 import Moment from 'react-moment';
-import { graphql, Query} from 'react-apollo';
-import { Card, ResourceItem, TextStyle, TextField, Filters, Page, Tabs, Layout, Link} from '@shopify/polaris';
+import { graphql, Query } from 'react-apollo';
+import { Card, ResourceItem, TextStyle, TextField, Filters, Page, Tabs, Layout, Link } from '@shopify/polaris';
 import VendorList from '../components/Lists/VendorList';
 
 const GET_QUERY = gql`
@@ -19,7 +19,7 @@ const GET_QUERY = gql`
     }
 `;
 
- function UsersList(){
+function UsersList() {
     const [selectedItems, setSelectedItems] = useState([]);
     const [taggedWith, setTaggedWith] = useState(null);
     const [queryValue, setQueryValue] = useState(null);
@@ -29,8 +29,8 @@ const GET_QUERY = gql`
     const handleTaggedWithChange = useCallback(
         (value) => setTaggedWith(value),
         [],
-      );
-    
+    );
+
     const handleQueryValueChange = useCallback(
         (value) => setQueryValue(value),
         [],
@@ -39,12 +39,12 @@ const GET_QUERY = gql`
     const handleTabChange = useCallback(
         (selectedTabIndex) => setSelected(selectedTabIndex),
         [],
-      );
+    );
 
-      const handleHeaderTabChange = useCallback(
+    const handleHeaderTabChange = useCallback(
         (selectedTabIndex) => setHeaderSelected(selectedTabIndex),
         [],
-      );
+    );
 
     const handleTaggedWithRemove = useCallback(() => setTaggedWith(null), []);
     const handleQueryValueRemove = useCallback(() => setQueryValue(null), []);
@@ -52,122 +52,122 @@ const GET_QUERY = gql`
         handleTaggedWithRemove();
         handleQueryValueRemove();
     }, [handleQueryValueRemove, handleTaggedWithRemove]);
-    
+
 
     const resourceName = {
-      singular: 'Vendor',
-      plural: 'Vendors',
+        singular: 'Vendor',
+        plural: 'Vendors',
     };
 
     const venderListTab = [
         {
-        id: 'all-venders',
-        content: 'All',
-        accessibilityLabel: 'All Venders',
-        panelID: 'all',
+            id: 'all-venders',
+            content: 'All',
+            accessibilityLabel: 'All Venders',
+            panelID: 'all',
         },
         {
-        id: 'pending-venders',
-        content: 'Pending Approval',
-        panelID: 'pending-approval',
+            id: 'pending-venders',
+            content: 'Pending Approval',
+            panelID: 'pending-approval',
         },
         {
-        id: 'approved-venders',
-        content: 'Approved',
-        panelID: 'approved',
+            id: 'approved-venders',
+            content: 'Approved',
+            panelID: 'approved',
         },
     ];
-    
+
     const headerTabs = [
         {
-          id:1,
-          content: 'Home',
-          panelID: 'home',
+            id: 1,
+            content: 'Home',
+            panelID: 'home',
         },
         {
-          id:2,
-          content: 'Invitations',
-          panelID: 'invitation',
+            id: 2,
+            content: 'Invitations',
+            panelID: 'invitation',
         },
         {
-            id:3,
+            id: 3,
             content: 'Orders',
             panelID: 'orders',
         },
         {
-            id:4,
+            id: 4,
             content: 'Products',
             panelID: 'products',
         },
         {
-            id:5,
+            id: 5,
             content: 'Payments',
             panelID: 'payments',
         },
         {
-            id:6,
+            id: 6,
             content: 'Vendors',
             panelID: 'vendors',
         },
         {
-            id:7,
+            id: 7,
             content: 'Settings',
             panelID: 'settings  ',
         },
 
-      ];
-    
+    ];
+
     const promotedBulkActions = [
-      {
-        content: 'Bulk Actions',
-        onAction: () => console.log('Todo: implement bulk edit'),
-      }
+        {
+            content: 'Bulk Actions',
+            onAction: () => console.log('Todo: implement bulk edit'),
+        }
     ];
 
     const filters = [
         {
-          key: 'taggedWith',
-          label: 'Filter',
-          filter: (
-            <TextField
-              label="Filter"
-              value={taggedWith}
-              onChange={handleTaggedWithChange}
-              labelHidden
-            />
-          ),
-          shortcut: true,
+            key: 'taggedWith',
+            label: 'Filter',
+            filter: (
+                <TextField
+                    label="Filter"
+                    value={taggedWith}
+                    onChange={handleTaggedWithChange}
+                    labelHidden
+                />
+            ),
+            shortcut: true,
         },
-      ];
-    
-      const appliedFilters = !isEmpty(taggedWith)
+    ];
+
+    const appliedFilters = !isEmpty(taggedWith)
         ? [
             {
-              key: 'Filter',
-              label: disambiguateLabel('taggedWith', taggedWith),
-              onRemove: handleTaggedWithRemove,
+                key: 'Filter',
+                label: disambiguateLabel('taggedWith', taggedWith),
+                onRemove: handleTaggedWithRemove,
             },
-          ]
+        ]
         : [];
-    
-        const filterControl = (
-            <Filters
+
+    const filterControl = (
+        <Filters
             queryValue={queryValue}
             filters={filters}
             appliedFilters={appliedFilters}
             onQueryChange={handleQueryValueChange}
             onQueryClear={handleQueryValueRemove}
             onClearAll={handleClearAll}
-            >
-            </Filters>
-        );
+        >
+        </Filters>
+    );
 
-        function renderItem(item) {
-            const {id, firstName, lastName, email, brandName, status, createDate } = item;
-            const shortcutActions = status
-                ? 'Pending':'Approved';
+    function renderItem(item) {
+        const { id, firstName, lastName, email, brandName, status, createDate } = item;
+        const shortcutActions = status
+            ? 'Pending' : 'Approved';
 
-            return (
+        return (
             <ResourceItem
                 id={id}
                 email={email}
@@ -175,57 +175,57 @@ const GET_QUERY = gql`
                 persistActions
             >
                 <h2>
-                <Link><TextStyle><strong>{brandName}</strong></TextStyle></Link>   
+                    <Link><TextStyle><strong>{brandName}</strong></TextStyle></Link>
                 </h2>
                 <p>
-                <TextStyle>Name:{firstName} {lastName}</TextStyle><br></br>
-                <TextStyle>Email:{email}</TextStyle>
+                    <TextStyle>Name:{firstName} {lastName}</TextStyle><br></br>
+                    <TextStyle>Email:{email}</TextStyle>
                 </p>
                 <p>
                     Onboarded on <Moment format="MMMM DD, YYYY  hh:mm">{createDate}</Moment>&nbsp;(GMT)
                 </p>
             </ResourceItem>
-            );
-        }
+        );
+    }
 
-        function resolveItemIds({id}) {
-            return id;
-        }
+    function resolveItemIds({ id }) {
+        return id;
+    }
 
-        function disambiguateLabel(key, value) {
-            switch (key) {
+    function disambiguateLabel(key, value) {
+        switch (key) {
             case 'taggedWith':
                 return `Tagged with ${value}`;
             default:
                 return value;
-            }
         }
-        
-        function isEmpty(value) {
-            if (Array.isArray(value)) {
-            return value.length === 0;
-            } else {
-            return value === '' || value == null;
-            }
-        }
+    }
 
-        return ( 
-         
-        <Page breadcrumbs={[{content: 'Login', url:'/login'}]} title="Vendors">
+    function isEmpty(value) {
+        if (Array.isArray(value)) {
+            return value.length === 0;
+        } else {
+            return value === '' || value == null;
+        }
+    }
+
+    return (
+
+        <Page breadcrumbs={[{ content: 'Login', url: '/login' }]} title="Vendors">
             <div>
                 <Tabs tabs={headerTabs} selected={headerSelected} onSelect={handleHeaderTabChange} fitted></Tabs>
             </div>
             <br></br>
-            <Query query={GET_QUERY}> 
+            <Query query={GET_QUERY}>
                 {({ loading, error, data, subscribeToMore }) => {
                     if (loading) return <p>Loading...</p>;
                     if (error) return <p>Error :</p>;
-                    
-                    return(
+
+                    return (
                         <Layout>
                             <Layout.Section>
                                 <Card>
-                                <Tabs tabs={venderListTab} selected={selected} onSelect={handleTabChange}></Tabs>
+                                    <Tabs tabs={venderListTab} selected={selected} onSelect={handleTabChange}></Tabs>
                                     <VendorList
                                         resourceName={resourceName}
                                         items={data.getUsers}
@@ -241,11 +241,11 @@ const GET_QUERY = gql`
                             </Layout.Section>
                             <Layout.Section secondary>
                                 <Card title="Quick Links">
-                                
-                                <Card.Section>
-                                    <Link url="/register"><h1><strong>Add Vendor</strong></h1></Link>
-                                    <p>Manage your Vendors</p>
-                                </Card.Section>
+
+                                    <Card.Section>
+                                        <Link url="/register"><h1><strong>Add Vendor</strong></h1></Link>
+                                        <p>Manage your Vendors</p>
+                                    </Card.Section>
                                 </Card>
                             </Layout.Section>
                         </Layout>
@@ -256,6 +256,6 @@ const GET_QUERY = gql`
     );
 }
 
-export default graphql(GET_QUERY,{
+export default graphql(GET_QUERY, {
     options: { fetchPolicy: 'network-only' },
 })(UsersList);
--- server/src/config/database.js
+++ server/src/config/database.js
@@ -5,7 +5,7 @@ mongoose.Promise = global.Promise;
 
 const url = process.env.URL;
 
-mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true,useUnifiedTopology: true }).then(
-  () => {console.log('Database is connected') },
-  err => { console.log('Can not connect to the database'+ err)}
+mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }).then(
+  () => { console.log('Database is connected') },
+  err => { console.log('Can not connect to the database' + err) }
 );
