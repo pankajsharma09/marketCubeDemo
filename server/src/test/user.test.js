1ed361704820c83ab5b80e8da06fd6c36bd2c1eb
premshani83 <prem.shani@successive.tech>
2019-10-18 20:17:05 +0530

    user authenticate case

--- server/src/test/user.test.js
+++ server/src/test/user.test.js
@@ -2,56 +2,104 @@ const EasyGraphQLTester = require('easygraphql-tester')
 const fs = require('fs')
 const path = require('path')
 const schemaCode = fs.readFileSync(path.join(__dirname, 'schema', '../../schema.graphql'), 'utf8')
+const resolver=require("../module/index");
+const mongoose = require('mongoose')
+const host = 'localhost:27017'
+const name = 'marketcubedb'
+const { expect } = require("chai");
 
-
-describe("Test User schema", () => {
+describe("Market Cube Test Cases", () => {
   let tester;
   beforeAll(() => {
-    tester = new EasyGraphQLTester(schemaCode);
+    mongoose.connect(`mongodb://${host}/${name}`, () => {
+      mongoose.connection.db.dropDatabase(() => {
+        mongoose.disconnect(() => {
+          console.log('closed')
+        })
+      })
+    })
+    tester = new EasyGraphQLTester(schemaCode,resolver);
   });
-  describe("Queries & Mutations", () => {
+  describe("User Not Found Test Case", () => {
+    it("User Not Found Test Case", async () => {
+      const query = `
+        mutation userAuthenticate($email: String, $password:String) {
+          userAuthenticate(email: $email, password: $password) {
+            email
+            password
+            response
+          }
+        }
+      `;
+  
+      const args = {
+        eamil: "premshani89@gmail.com",
+        password: "test@123"
+      };
+  
+      const result = await tester.graphql(query, {}, {}, args);
+      console.log(result);
+      expect(result.data.userAuthenticate.response).to.be.eq("INVALID");
+      //expect(result.data.license.body).to.be.eq("This is a test license");
+      //expect(result.data.license.description).to.be.eq(`This is a description with key ${args.key}`
+      //);
+    });
 
-    test("Should get all the fields on getUsers", () => {
+    //test case for the getuser query
+    test("Valid User schema with valid field", () => {
       const query = `
         {
           getUsers {
             email
+            password
+            isVendor
+            brandName
+            status
+            createDate
+            updateDate
             firstName
             lastName
-            brandName
           }
         }
       `;
-      // First arg: true, the query is valid
-      // Second arg: query to test
-      tester.test(true, query);
-
+      tester.test(true,query);
+      
     });
-    test("Should be a valid mutation", () => {
+
+
+    
+
+     test("Should be a valid mutation for add user", () => {
       const mutation = `
         mutation addUser {
           addUser {
             email
             password
+            isVendor
+            brandName
+            status
+            createDate
+            updateDate
             firstName
+            lastName
           }
         }
       `;
-      console.log("Muation ", tester.gqlshema);
-      // First arg: true, the mutation is valid
-      // Second arg: mutation to test
-      // Third argument: Input value
-      tester.test(true, mutation, {
-        email: "demo@demo.com",
-        firstName: "demo",
-        passowrd: "demo name"
-      });
-      tester.test(true, mutation, {
-        email: null,
-        firstName: "demo",
-        passowrd: "demo name"
-
+      
+       tester.test(true , mutation, {
+        email:"prem@gm.com",
+        password: "test",
+        isVendor: true ,
+        brandName:"ZeeTest",
+        status:1,
+        createDate:"2019-10-18",
+        updateDate:"2019-10-18",
+        firstName:"prem",
+        lastName:"shani"
       });
     });
   });
+  
 });
+
+
